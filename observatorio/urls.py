73eb0ef76417a642523bc8520a69dc7ba2740ad7
sbonacci33 <sbonacci89@gmail.com>
2025-06-06 19:04:51 -0300

    Agrega vista y menu para consulta IA

--- observatorio/urls.py
+++ observatorio/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("crear/", views.InformeCreateView.as_view(), name="crear_informe"),
     path("informes/", views.InformeListView.as_view(), name="listar_informes"),
     path("buscar/", views.buscar_informes, name="buscar_informes"),
+    path("consulta-ia/", views.consulta_ia, name="consulta_ia"),
     path("medios/", views.MedioAmigoListView.as_view(), name="medios"),
     path("suscribirse/", views.suscribirse, name="suscribirse"),
     path("logout/", views.logout_view, name="logout_user"),
