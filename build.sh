535fe9d76cad1e3f8a534609d3d59329b24a84d1
Santiago Bonacci <sbonacci89@gmail.com>
2025-06-22 13:48:42 -0300

    Fix: instalar dependencias en build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+# Instalar dependencias
+pip install -r requirements.txt
+
 # Migraciones y archivos estáticos
 python manage.py migrate
 python manage.py collectstatic --noinput
-
